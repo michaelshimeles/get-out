eecb626dbaef4a153951d7e50605ed627a13b8fe
Michael Shimeles <michaelwasihun96@gmail.com>
2022-12-01 23:48:58 -0500

    Added Scan Card button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar/NavBar";
 import HeroSection from "./components/HeroSection/HeroSection"
+import ScanCard from "./components/ScanCard/ScanCard"
 import "./App.scss";
 function App() {
   return (
-    <div>
+    <div className="app">
       <NavBar />
       <HeroSection />
+      <ScanCard />
     </div>
   );
 }
