fa9ef0669a5625d808db22dc126ae4c127873766
David-McCaig <davidmccaig1@gmail.com>
2022-12-02 00:28:04 -0500

    create card for main page

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -10,19 +10,52 @@ function Card() {
                 </div>
 
                 <div className="card__container">
-                    <h2 className="card__subtitle">For You</h2>
-                    <p>Browse some rewards we think you'll love.</p>
-                   
-                        <div className="card__button" >
-                            <div className="card__button-text">Redeem</div>
-                        </div>
-                    
+                    <h2 className="card__subtitle">subhead</h2>
+                    <p className="card__text">Browse some rewards we think you'll love.Browse some rewards we think you'll love.</p>
+
+                    <div className="card__button" >
+                        <div className="card__button-text">Redeem</div>
+                    </div>
+
                 </div>
 
             </div>
+
+            <div className="card">
+                <div className="card__image">
+
+                </div>
+
+                <div className="card__container">
+                    <h2 className="card__subtitle">subhead</h2>
+                    <p className="card__text">Browse some rewards we think you'll love.Browse some rewards we think you'll love.</p>
+
+                    <div className="card__button" >
+                        <div className="card__button-text">Redeem</div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className="card">
+                <div className="card__image">
+
+                </div>
+
+                <div className="card__container">
+                    <h2 className="card__subtitle">subhead</h2>
+                    <p className="card__text">Browse some rewards we think you'll love.Browse some rewards we think you'll love.</p>
+
+                    <div className="card__button" >
+                        <div className="card__button-text">Redeem</div>
+                    </div>
+
+                </div>
+
+            </div>
+
         </>
-
-
     );
 }
 
