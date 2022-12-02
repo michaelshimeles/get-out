8af41fb61ef2b4309dc269bf8c4c227bcc8ace38
David-McCaig <davidmccaig1@gmail.com>
2022-12-02 00:59:12 -0500

    add image to Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,5 @@
 import "./Card.scss"
+import greyBox from "../../assets/images/Frame 155 (1).png"
 
 function Card() {
 
@@ -6,7 +7,24 @@ function Card() {
         <>
             <div className="card">
                 <div className="card__image">
+                <img src={greyBox} alt="greybox"/>
+                </div>
 
+                <div className="card__container">
+                    <h2 className="card__subtitle">subhead</h2>
+                    <p className="card__text">Browse some rewards we think you'll love.Browse some rewards we think you'll love.</p>
+
+                    <div className="card__button" >
+                        <div className="card__button-text">Redeem</div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className="card">
+                    <div className="card__image">
+                <img src={greyBox} alt="greybox"/>
                 </div>
 
                 <div className="card__container">
@@ -23,24 +41,7 @@ function Card() {
 
             <div className="card">
                 <div className="card__image">
-
-                </div>
-
-                <div className="card__container">
-                    <h2 className="card__subtitle">subhead</h2>
-                    <p className="card__text">Browse some rewards we think you'll love.Browse some rewards we think you'll love.</p>
-
-                    <div className="card__button" >
-                        <div className="card__button-text">Redeem</div>
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div className="card">
-                <div className="card__image">
-
+                        <img src={greyBox} alt="greybox"/>
                 </div>
 
                 <div className="card__container">
