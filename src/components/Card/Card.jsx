0f0769bc08cbbf8cdb102bca0894976ecd6ce4a4
Michael Shimeles <michaelwasihun96@gmail.com>
2022-12-02 01:14:16 -0500

    Fixing card component so it doesn't crash with footer

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,63 +1,28 @@
-import "./Card.scss"
-import greyBox from "../../assets/images/Frame 155 (1).png"
+import "./Card.scss";
+import greyBox from "../../assets/images/Frame 155 (1).png";
 
 function Card() {
+  return (
+    <>
+      <div className="card">
+        <div className="card__image">
+          <img src={greyBox} alt="greybox" />
+        </div>
 
-    return (
-        <>
-            <div className="card">
-                <div className="card__image">
-                <img src={greyBox} alt="greybox"/>
-                </div>
+        <div className="card__container">
+          <h2 className="card__subtitle">subhead</h2>
+          <p className="card__text">
+            Browse some rewards we think you'll love.Browse some rewards we
+            think you'll love.
+          </p>
 
-                <div className="card__container">
-                    <h2 className="card__subtitle">subhead</h2>
-                    <p className="card__text">Browse some rewards we think you'll love.Browse some rewards we think you'll love.</p>
-
-                    <div className="card__button" >
-                        <div className="card__button-text">Redeem</div>
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div className="card">
-                    <div className="card__image">
-                <img src={greyBox} alt="greybox"/>
-                </div>
-
-                <div className="card__container">
-                    <h2 className="card__subtitle">subhead</h2>
-                    <p className="card__text">Browse some rewards we think you'll love.Browse some rewards we think you'll love.</p>
-
-                    <div className="card__button" >
-                        <div className="card__button-text">Redeem</div>
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div className="card">
-                <div className="card__image">
-                        <img src={greyBox} alt="greybox"/>
-                </div>
-
-                <div className="card__container">
-                    <h2 className="card__subtitle">subhead</h2>
-                    <p className="card__text">Browse some rewards we think you'll love.Browse some rewards we think you'll love.</p>
-
-                    <div className="card__button" >
-                        <div className="card__button-text">Redeem</div>
-                    </div>
-
-                </div>
-
-            </div>
-
-        </>
-    );
+          <div className="card__button">
+            <div className="card__button-text">Redeem</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Card;
