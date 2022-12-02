6fa8231f023be437192e7424b18885a8a638b65b
Michael Shimeles <michaelwasihun96@gmail.com>
2022-12-02 14:57:42 -0500

    Added alt text to images in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,29 +13,29 @@ const Footer = () => {
         <div className="footer__top">
           <Link to="/" className="footer__link">
             <div className="footer__home">
-              <img src={home} />
+              <img src={home} alt="Home Icon"/>
               <p>Home</p>
             </div>
           </Link>
           <Link to="/rewards" className="footer__link">
             <div className="footer__scan">
-              <img src={scan} />
+              <img src={scan} alt="Scan Icon"/>
               <p>Scan</p>
             </div>
           </Link>
 
           <div className="footer__offers">
-            <img src={offers} />
+            <img src={offers} alt="Offers Icon"/>
             <p>Offers</p>
           </div>
           <div className="footer__bazaar">
             {" "}
-            <img src={bazaar} />
+            <img src={bazaar} alt="Bazaar Icon"/>
             <p>Bazaar</p>
           </div>
         </div>
         <div className="footer__bar">
-          <img src={bar} />
+          <img src={bar} alt="Swipe Up Icon"/>
         </div>
       </div>
     </footer>
